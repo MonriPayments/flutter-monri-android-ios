62d66f4d6064f2810e06f9152f20acfedcd68cc5
adnanomerovic <1812992171632>
2021-11-01 13:47:48 +0100

    WP-801 Working on Flutter SDK
    Added form for easiest testing.
    WP-801

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
+useful commands:
+flutter pub run build_runner build
+flutter pub run build_runner watch --delete-conflicting-outputs
---
+++ example/lib/string_messages.dart
@@ -0,0 +1,8 @@
+class ValidationMessages{
+  static const filedRequired = "Field is required";
+  static const invalidCvv = "CVV is invalid";
+  static const invalidMonth = "Expiry month is invalid";
+  static const invalidYear = "Expiry year is invalid";
+  static const invalidCardNumber = "Card numer is invalid";
+  static const invalidCard = "Card is invalid";
+}
